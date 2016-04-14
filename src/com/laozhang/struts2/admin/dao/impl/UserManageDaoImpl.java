c0e4c9462b6279f0085449341bef6b5600958b7f
zhangchengfu <compowerxp@163.com>
2016-04-14 17:06:02 +0800

    2016-04-14

---
+++ src/com/laozhang/struts2/admin/dao/impl/UserManageDaoImpl.java
@@ -0,0 +1,40 @@
+package com.laozhang.struts2.admin.dao.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.laozhang.struts2.admin.dao.UserManageDao;
+import com.laozhang.struts2.admin.model.RoleInfo;
+import com.laozhang.struts2.admin.model.UserInfo;
+import com.laozhang.struts2.base.dao.impl.BaseDaoImpl;
+
+public class UserManageDaoImpl extends BaseDaoImpl implements UserManageDao {
+
+	@Override
+	public UserInfo findUserById(String id) {
+		List<UserInfo> list = findAllBy(UserInfo.class, "userId", id);
+		return (list == null ? null : list.get(0));
+	}
+
+	@Override
+	public List<RoleInfo> getRoleListByUserId(Map<String, String> map) {
+		return (List<RoleInfo>)findListByCombinedHsql("getRoleListByUserId", map);
+	}
+
+	@Override
+	public boolean addUser(UserInfo userInfo) {
+		try {
+			saveObject(userInfo);
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
+	}
+
+	@Override
+	public UserInfo searchUserById(Long id) {
+		return (UserInfo) findBy(UserInfo.class, "id", id);
+	}
+
+}
--- src/com/laozhang/struts2/security/MyUserDetailServiceImpl.java
+++ src/com/laozhang/struts2/security/MyUserDetailServiceImpl.java
@@ -1,16 +1,49 @@
 package com.laozhang.struts2.security;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class MyUserDetailServiceImpl implements UserDetailsService {
+import com.laozhang.struts2.admin.dao.UserManageDao;
+import com.laozhang.struts2.admin.model.RoleInfo;
+import com.laozhang.struts2.admin.model.UserInfo;
 
-	@Override
-	public UserDetails loadUserByUsername(String arg0)
-			throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+public class MyUserDetailServiceImpl implements UserDetailsService {
+	
+	private UserManageDao userManageDao;
+
+	public UserManageDao getUserManageDao() {
+		return userManageDao;
 	}
 
+	public void setUserManageDao(UserManageDao userManageDao) {
+		this.userManageDao = userManageDao;
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
+		UserInfo user = userManageDao.findUserById(username);
+		
+		return null;
+	}
+	
+	//取得用户的权限
+	private Set<GrantedAuthority> obtionGrantedAuthorities(UserInfo user) {
+		Set<GrantedAuthority> set = new HashSet<GrantedAuthority>();
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("userId", user.getUserId());
+		List<RoleInfo> roles = userManageDao.getRoleListByUserId(map);
+		for (RoleInfo role : roles) {
+		}
+		return null;
+	}
 }
